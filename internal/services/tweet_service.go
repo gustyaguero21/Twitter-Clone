fd24fbc4b0a6816f65cfc57fdf7888c1350f8cd2
Gustavo Aguero <gustyaguero21@@gmail.com>
2024-11-01 23:53:12 -0300

    Commit: Adding timeline

--- internal/services/tweet_service.go
+++ internal/services/tweet_service.go
@@ -3,8 +3,6 @@ package services
 import (
 	"context"
 	"fmt"
-	"sort"
-	"sync"
 	"time"
 	"twitter-clone/cmd/config"
 	"twitter-clone/internal/models"
@@ -50,44 +48,14 @@ func (ts *TweetServices) validate(content string) error {
 }
 
 func (ts *TweetServices) ShowTimeline(username string) ([]models.Tweets, error) {
+
 	tweetRepo := repository.NewPostRepository(*ts.Repo)
 
-	followingUsernames, err := tweetRepo.GetFollowingUsernames(username)
-	if err != nil {
-		return nil, err
+	tweetList, tweetErr := tweetRepo.GetTimeline(username)
+
+	if tweetErr != nil {
+		return []models.Tweets{}, tweetErr
 	}
+	return tweetList, nil
 
-	var wg sync.WaitGroup
-	tweetsChan := make(chan []models.Tweets, len(followingUsernames))
-
-	for _, followedUser := range followingUsernames {
-		wg.Add(1)
-		go func(user string) {
-			defer wg.Done()
-			tweets, err := tweetRepo.GetTimeline(user)
-			if err == nil {
-				tweetsChan <- tweets
-			}
-		}(followedUser)
-	}
-
-	go func() {
-		wg.Wait()
-		close(tweetsChan)
-	}()
-
-	var allTweets []models.Tweets
-
-	for tweets := range tweetsChan {
-		allTweets = append(allTweets, tweets...)
-	}
-
-	sort.Slice(allTweets, func(i, j int) bool {
-		if allTweets[i].User == allTweets[j].User {
-			return allTweets[i].PostedAt.After(allTweets[j].PostedAt)
-		}
-		return allTweets[i].User < allTweets[j].User
-	})
-
-	return allTweets, nil
 }
